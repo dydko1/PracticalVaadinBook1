53857a670cc6788f12e7937e73c5a008216d4083
dydko1 <miroslaw.dyduch@gmail.com>
2021-10-05 07:14:53 +0200

    tmp 05.10.2021

---
+++ src/main/java/com/dydko/ch03/PaddingMarginSpacingView.java
@@ -0,0 +1,28 @@
+package com.dydko.ch03;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("padding-margin-spacing")
+public class PaddingMarginSpacingView extends Composite<Component> {
+    @Override
+    protected Component initContent() {
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.getStyle().set("border", "1px solid");
+        layout.setPadding(false);
+        layout.setMargin(false);
+        layout.setMargin(false);
+        layout.setSpacing(false);
+        layout.add(
+                new Paragraph("Toggle:"),
+                new Button("Padding", e -> layout.setPadding(!layout.isPadding())),
+                new Button("Margin", e -> layout.setMargin(!layout.isMargin())),
+                new Button("Spacing", e -> layout.setSpacing(!layout.isSpacing()))
+        );
+        return layout;
+    }
+}
