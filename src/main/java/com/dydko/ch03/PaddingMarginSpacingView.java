76868da1504cb1c3a10683248d05fdd56a8eb270
dydko1 <miroslaw.dyduch@gmail.com>
2021-10-05 21:28:40 +0200

    tmp 05.10.2021

--- src/main/java/com/dydko/ch03/PaddingMarginSpacingView.java
+++ src/main/java/com/dydko/ch03/PaddingMarginSpacingView.java
@@ -2,6 +2,7 @@ package com.dydko.ch03;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -12,6 +13,10 @@ public class PaddingMarginSpacingView extends Composite<Component> {
     @Override
     protected Component initContent() {
         HorizontalLayout layout = new HorizontalLayout();
+        Button button1 = new Button("I'm 100%");
+        Button button2 = new Button("I'm 2in%");
+        button1.setWidth("50%");
+        button2.setWidth("2in");
         layout.getStyle().set("border", "1px solid");
         layout.setPadding(false);
         layout.setMargin(false);
@@ -21,7 +26,11 @@ public class PaddingMarginSpacingView extends Composite<Component> {
                 new Paragraph("Toggle:"),
                 new Button("Padding", e -> layout.setPadding(!layout.isPadding())),
                 new Button("Margin", e -> layout.setMargin(!layout.isMargin())),
-                new Button("Spacing", e -> layout.setSpacing(!layout.isSpacing()))
+                new Button("Spacing", e -> layout.setSpacing(!layout.isSpacing())),
+                button1,
+                button2,
+                new Button("I'm 1 in%", e->layout.setWidth("5px"))
+
         );
         return layout;
     }
