cb6e5c321e48c2f92538583f3f0e01ad646fcc9e
dydko1 <miroslaw.dyduch@gmail.com>
2021-10-07 21:12:20 +0200

    07.10.2021

---
+++ src/main/java/com/dydko/ch03/FlexDiretion1.java
@@ -0,0 +1,24 @@
+package com.dydko.ch03;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("flex-wrap")
+public class FlexDiretion1 extends Composite<Component> {
+    protected Component initContent() {
+        FlexLayout layout = new FlexLayout();
+        for (int i = 0; i < 100; i++) {
+            layout.add(new Button("Button " + i));
+        }
+        layout.setFlexDirection(FlexLayout.FlexDirection.ROW);
+        layout.setFlexWrap(FlexLayout.FlexWrap.WRAP);
+        layout.setAlignContent(FlexLayout.ContentAlignment.SPACE_BETWEEN);
+
+        layout.setSizeFull();
+        return layout;
+    }
+
+}
---
+++ src/main/java/com/dydko/ch03/ScrollerView.java
@@ -0,0 +1,23 @@
+package com.dydko.ch03;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("scroller")
+public class ScrollerView extends Composite<Component> {
+    @Override
+    protected Component initContent() {
+        VerticalLayout layout = new VerticalLayout();
+        for (int i = 0; i < 100; i++) {
+            layout.add(new Button("Button " + i));
+        }
+        Scroller scroller = new Scroller(layout);
+        scroller.setHeight("400px");
+        scroller.getStyle().set("border", "1px solid");
+        return scroller;
+    }
+}
