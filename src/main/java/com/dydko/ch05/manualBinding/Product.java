e68aed5c5ff3417daf50adfd1a5ce68bd46b2abe
dydko1 <miroslaw.dyduch@gmail.com>
2021-10-16 08:14:27 +0200

    temporary manual Binding

---
+++ src/main/java/com/dydko/ch05/manualBinding/Product.java
@@ -0,0 +1,33 @@
+package com.dydko.ch05.manualBinding;
+
+public class Product {
+    private String name;
+    private boolean available = true;
+    private Manufacturer manufacturer;
+
+    public Product(){};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}
